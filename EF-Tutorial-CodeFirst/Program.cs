fa5fe300bf9c1a512121184ffd56004c37db1e29
Michael Walker <walkerrepair255@yahoo.com>
2021-10-07 15:45:30 -0400

    ordercontroller done with program code to demo

--- EF-Tutorial-CodeFirst/Program.cs
+++ EF-Tutorial-CodeFirst/Program.cs
@@ -1,14 +1,20 @@
 ﻿using SalesDbLib.Controllers;
 using System;
+using System.Threading.Tasks;
 
 namespace EF_Tutorial_CodeFirst
 {
     class Program
     {
-        static void Main(string[] args)
+        async static Task Main(string[] args)
         {
             var custCtrl = new CustomersController();
-            var customers = custCtrl.GetAll();
+            var customers = await custCtrl.GetAll();
+            customers.ForEach(c => Console.WriteLine(c.Name));
+
+            var orderCtrl = new OrdersController();
+            var orders = await orderCtrl.GetAll();
+            orders.ForEach(o => Console.WriteLine($"{o.Description} | {o.Total} | {o.Customer.Name}"));
         }
     }
 }
